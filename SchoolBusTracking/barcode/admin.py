a219fcfc5b71b402c8aed3d0c510c9d42d47cdb5
benendless <fonyuybernard257@gmail.com>
2025-05-11 09:08:09 +0100

    QTCODE

--- SchoolBusTracking/barcode/admin.py
+++ SchoolBusTracking/barcode/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Check_info
 
 # Register your models here.
+admin.site.register(Check_info)
--- SchoolBusTracking/barcode/models.py
+++ SchoolBusTracking/barcode/models.py
@@ -8,6 +8,7 @@ class Extractedimage(models.Model):
 class Check_info(models.Model):
     name = models.CharField(max_length=100)
     passport_image = models.ImageField(upload_to='extracted_images/')
+    qr_code = models.ImageField(upload_to='qr_codes',blank=True)
 
     def __str__(self):
         return f"Extracted Image {self.id}"
